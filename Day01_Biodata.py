f3076cd54c1e04cf19d83524271a0c10f9d06c22
Chaerulcp <indonesianvidios@gmail.com>
2022-10-11 20:36:19 +0800

    Day 1

---
+++ Day01_Biodata.py
@@ -0,0 +1,20 @@
+
+Nama = "Chaerul Candra Pranugrah"
+Tempat_Lahir = "Makassar"
+Tgl_lahir = "16 November 2001"
+Agama = "Islam"
+No_telp ="085341912927"
+Alamat = "Lembang, Majene"
+
+print("---------------------------------------")
+print("-------------- BIODATA ----------------")
+print("---------------------------------------")
+print("Nama          : ", Nama)
+print("Tempat Lahir  : ", Tempat_Lahir)
+print("Tanggal Lahir : ", Tgl_lahir)
+print("Agama         : ", Agama)
+print("Nomor Telpon  : ",No_telp)
+print("Alamat        : ",Alamat)
+print("---------------------------------------")
+print("---------------------------------------")
+
