48f850758d959338661e75763edd6a76ff9d6cd6
Chaerulcp <indonesianvidios@gmail.com>
2022-10-13 20:12:16 +0800

    Day 3

---
+++ Day03_Input_Data_Dari_User.py
@@ -0,0 +1,13 @@
+# untuk menginput data dari user kita bisa menggunakan cara berikut
+nama = input("Masukan Nama : ") # keterangan apapun yang di input outputnya akan menjadi tipe data str
+
+# untuk bisa output nya berupa tipe data lain kita bisa melalukan casting tipe data
+umur = int(input("Masukan Umur : "))
+berat_badan = float(input("Masukan berat Badan : "))
+
+print("Nama        : ", nama)
+print("Umur        : ", umur,"Tahun")
+print("Berat Badan : ", berat_badan,"Kg")
+
+
+
