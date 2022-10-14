5722650a7f1d08535cacb0ebc15035cfed17ff9f
Chaerulcp <indonesianvidios@gmail.com>
2022-10-14 20:44:06 +0800

    Day 4

---
+++ Day04_Operasi_Aritmatika.py
@@ -0,0 +1,34 @@
+# Operasi Aritmatika pada python
+
+a = 10
+b = 6
+
+# Penjumlahan ( + )
+print("========== Operasi Penjumlahan =======")
+hasil = a + b
+print(a,'+' ,b, '=', hasil)
+
+# Pengurangan ( - )
+print("========== Operasi Pengurangan =======")
+hasil = a - b
+print(a,'-' ,b, '=', hasil)
+
+# Perkalian ( * )
+print("========== Operasi Perkalian =======")
+hasil = a * b
+print(a,'x' ,b, '=', hasil)
+
+# Penjumlahan ( / ) ket: untuk operasi pembagian otomatis outputnya jadi float
+print("========== Operasi Pembagian =======")
+hasil = a / b
+print(a,'/' ,b, '=', hasil)
+
+# Modulus (%) ket.modulus adalah sisa bagi
+print("========== Operasi Modulus =======")
+hasil = a ** b
+print(a,'%' ,b, '=', hasil)
+
+# Eksponen (pangkat) **
+print("========== Operasi Eksponen =======")
+hasil = a ** b
+print(a,'**' ,b, '=', hasil)
