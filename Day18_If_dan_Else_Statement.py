cd9f5c4129bf9a8237fc259a640686bf1218ce33
Chaerulcp <indonesianvidios@gmail.com>
2022-10-28 23:55:09 +0800

    Day 18

---
+++ Day18_If_dan_Else_Statement.py
@@ -0,0 +1,27 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# 1. program if inline
+if nama=="chaerul" : print("Kamu keren!!!!")
+print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+if nama=="chaerul":
+	print("kamu keren abieeeez!")
+	print("kamu juga keren banget!")
+print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama=="chaerul":
+	print("hai chaerul, si keren!!!")
+else:
+	print("Ah kamu bukan chaerul, kamu gak keren! :(")
+
+print("akhir dari program")
