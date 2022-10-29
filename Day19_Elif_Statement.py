68818ca6c1eda4a4da2eb53d02fe892e32f4fc3b
Chaerulcp <indonesianvidios@gmail.com>
2022-10-30 00:11:09 +0800

    Day 19

---
+++ Day19_Elif_Statement.py
@@ -0,0 +1,14 @@
+# ELIF = else if statement
+
+nama = input("Nama kamyu siapa? ")
+
+if nama == "chaerul": # kondisi 1
+	print("Hai chaerul!") # aksi true 1
+elif nama == "irul": # kondisi 2
+	print("Hai si kece bangeeeets!!") # aksi true 2
+elif nama == "rul": # kondisi 3
+	print("Hai rul!") # aksi true 3
+else:
+	print("au ah gak kenal!!!") # aksi false
+
+print("ini adalah akhir dari program")
