0dd77bc654a58098b98dab1f7032dd13b9fe6152
Chaerulcp <indonesianvidios@gmail.com>
2022-11-16 01:09:13 +0800

    Day 35

---
+++ Day35_Dictionary.py
@@ -0,0 +1,22 @@
+# list -> array, mengakses dengan 
+# menggunakan index
+
+data_list = ['chaerul','darwin','ramadhan']
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array
+# identifier -> key
+
+data_dict = {
+	'key':'value',
+	'cp':'chaerul',
+	'tg':'darwin',
+	'dg':'ramadhan',
+	'nmbr':100,
+	'list':data_list,
+}
+
+print(data_dict['tg'])
+print(data_dict['nmbr'])
+print(data_dict['list'])
