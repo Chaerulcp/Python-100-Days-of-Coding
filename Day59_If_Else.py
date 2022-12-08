d0f4d642ec83f2b058673e654ea621389725b96a
Chaerulcp <indonesianvidios@gmail.com>
2022-12-08 20:23:28 +0800

    Day 59

---
+++ Day59_If_Else.py
@@ -0,0 +1,21 @@
+# if else statment 
+
+# if 
+# kondisi
+# aksi
+
+nama = input("siapa teman kamu? : ")
+
+if nama == "chaerul" :
+  print(f"{nama} dia adalah teman kamu")
+  
+elif nama == "rama" : 
+  print(f"{nama} dia adalah teman kamu")
+  
+elif nama == "darwin" :  
+  print(f"{nama} dia adalah teman kamu")
+  
+else :
+  print(f"{nama} bukan teman kamu")
+
+print("selesai")
