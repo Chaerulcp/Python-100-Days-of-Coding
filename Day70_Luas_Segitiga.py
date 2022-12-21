e1dc5a0420b5adfa48ccf6a49652fe9d0e708739
Chaerulcp <indonesianvidios@gmail.com>
2022-12-21 17:20:20 +0800

    Day 70

---
+++ Day70_Luas_Segitiga.py
@@ -0,0 +1,11 @@
+# Program untuk menghitung luas segitiga
+
+# Input dari user
+alas = float(input("Masukkan nilai alas segitiga: "))
+tinggi = float(input("Masukkan nilai tinggi segitiga: "))
+
+# Menghitung luas segitiga
+luas = (alas * tinggi) / 2
+
+# Menampilkan hasil
+print("Luas segitiga adalah", luas)
