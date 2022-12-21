c304c2d0499d49da98a3399a60e7d16d82c7b751
Chaerulcp <indonesianvidios@gmail.com>
2022-12-21 17:22:10 +0800

    Day 71

---
+++ Day71_Luas_Persegi_Panjang.py
@@ -0,0 +1,11 @@
+# Program untuk menghitung luas persegi panjang
+
+# Input dari user
+panjang = float(input("Masukkan nilai panjang persegi panjang: "))
+lebar = float(input("Masukkan nilai lebar persegi panjang: "))
+
+# Menghitung luas persegi panjang
+luas = panjang * lebar
+
+# Menampilkan hasil
+print("Luas persegi panjang adalah", luas)
