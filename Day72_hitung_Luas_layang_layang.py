179983b4508baf7e2072c622b634c9db0d0b9d9e
Chaerulcp <indonesianvidios@gmail.com>
2022-12-21 17:23:11 +0800

    Day 72

---
+++ Day72_hitung_Luas_layang_layang.py
@@ -0,0 +1,11 @@
+# Program untuk menghitung luas layang-layang
+
+# Input dari user
+d1 = float(input("Masukkan nilai diagonal 1 layang-layang: "))
+d2 = float(input("Masukkan nilai diagonal 2 layang-layang: "))
+
+# Menghitung luas layang-layang
+luas = (d1 * d2) / 2
+
+# Menampilkan hasil
+print("Luas layang-layang adalah", luas)
