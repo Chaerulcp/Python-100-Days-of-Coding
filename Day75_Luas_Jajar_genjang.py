c0fd57c527052df403e39ae6fad586fbd1286b8f
Chaerulcp <indonesianvidios@gmail.com>
2022-12-25 21:45:41 +0800

    Day 75

---
+++ Day75_Luas_Jajar_genjang.py
@@ -0,0 +1,9 @@
+# Meminta input panjang alas dan tinggi dari pengguna
+alas = float(input("Masukkan panjang alas: "))
+tinggi = float(input("Masukkan tinggi jajar genjang: "))
+
+# Menghitung luas jajar genjang menggunakan rumus di atas
+luas = alas * tinggi
+
+# Menampilkan hasilnya ke layar
+print("Luas jajar genjang adalah", luas)
