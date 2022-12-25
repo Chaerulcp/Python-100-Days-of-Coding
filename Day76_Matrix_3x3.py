a4419e8bb6ddafffeb7b9285c13884f33e3e2e7f
Chaerulcp <indonesianvidios@gmail.com>
2022-12-25 21:46:16 +0800

    Day 76

---
+++ Day76_Matrix_3x3.py
@@ -0,0 +1,16 @@
+# Membuat matriks 3x3 menggunakan list
+matriks = [[1, 2, 3],
+           [4, 5, 6],
+           [7, 8, 9]]
+
+# Menampilkan matriks ke layar
+print(matriks)
+
+# Menampilkan elemen di baris ke-2 kolom ke-3
+print(matriks[1][2])
+
+# Mengubah elemen di baris ke-3 kolom ke-1 menjadi 10
+matriks[2][0] = 10
+
+# Menampilkan matriks ke layar setelah diubah
+print(matriks)
